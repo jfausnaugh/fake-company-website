2988333da0695a403ff3f1e17acbab1b507fc288
Juliette Fausnaugh <j.d.land.16@gmail.com>
2024-02-22 14:45:47 -0500

    Initial Commit

---
+++ Home_Page.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+st.title("The Best Company")
+
+content = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra dictum ligula, 
+vestibulum finibus augue finibus vel. Nulla facilisi. Curabitur sagittis condimentum mi, 
+et tempus quam aliquet in. Ut dictum luctus facilisis. Aenean posuere non quam non dignissim.
+ Sed eget tempus orci. Aliquam condimentum consectetur nisi et iaculis. Quisque commodo,
+  leo in faucibus hendrerit, mi dolor pretium odio, ut ultricies nisl dui ut risus.
+   Maecenas sed mauris eros. Maecenas sit amet risus sit amet dolor vulputate mattis 
+   id ut odio. Mauris et tellus at eros luctus ultricies semper in turpis."""
+
+st.write(content)
+
+st.header("Our Team")
+
+col1, col2, col3 = st.columns(3)
+
+df = pandas.read_csv("data_company.csv")
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.image("company_images/" + row["image"])
+        st.subheader(row["first name"].title() + " " + row["last name"].title())
+        st.write(row["role"])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.image("company_images/" + row["image"])
+        st.subheader(row["first name"].title() + " " + row["last name"].title())
+        st.write(row["role"])
+
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.image("company_images/" + row["image"])
+        st.subheader(row["first name"].title() + " " + row["last name"].title())
+        st.write(row["role"])
+
---
+++ pages/Contact_the_Team.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas
+
+from send_inquiry import send_inquiry
+st.header("Contact the Team")
+
+df = pandas.read_csv("topics.csv")
+
+with st.form(key="contact_form"):
+    user_email = st.text_input("Your email address")
+    user_topic = st.selectbox("What topic do you want to discuss?", df["topic"])
+    raw_user_message = st.text_area("Your message")
+    message = f"""\
+Subject: New inquiry from {user_email}
+
+From: {user_email}
+Topic: {user_topic}
+{raw_user_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_inquiry(message)
+        st.info("Your message was sent successfully!")
